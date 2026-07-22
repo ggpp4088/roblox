842eee78fcf87e7f28e420650f7bcaf395c69d10
ggpp4088 <1478497303@qq.com>
2026-07-23 01:46:29 +0800

    Add files via upload

--- kl.lua
+++ kl.lua
@@ -4,4 +4,6 @@ elseif game.PlaceId == 1537690962 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/boos.lua'))("")
 elseif game.PlaceId == 189707 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/bss.lua'))("")
+elseif game.PlaceId == 13822889 then
+    loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/fz/%E9%A3%9E%E8%A1%8C.lua"))()'))("")
 end
