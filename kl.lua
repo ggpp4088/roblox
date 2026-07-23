5a313c49ec9bee1afe213765e1de91800319b2f6
ggpp4088 <1478497303@qq.com>
2026-07-23 19:45:56 +0800

    更新 kl.lua

--- kl.lua
+++ kl.lua
@@ -1,9 +1,10 @@
 if game.PlaceId == 13822889 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/1.lua'))("")
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/ddcs.lua'))("")
 elseif game.PlaceId == 1537690962 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/boos.lua'))("")
 elseif game.PlaceId == 189707 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/bss.lua'))("")
-elseif game.PlaceId == 537413528 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/fz/%E9%A3%9E%E8%A1%8C.lua'))("")
+elseif game.PlaceId == 3956818381 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/bss.lua'))("")
 end
