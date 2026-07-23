cc453ce675697154348f17f5515dacbab69c8d5d
ggpp4088 <1478497303@qq.com>
2026-07-23 20:57:05 +0800

    更新 kl.lua

--- kl.lua
+++ kl.lua
@@ -7,7 +7,7 @@ elseif game.PlaceId == 189707 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/bss.lua'))("")
 elseif game.PlaceId == 3956818381 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/hub/bss.lua'))("")
-elseif game.PlaceId == 4588604953 then
+elseif game.PlaceId == 8343259840 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ggpp4088/roblox/refs/heads/main/%E6%B3%A8%E5%B0%84%E5%99%A8'))("")
     loadstring(game:HttpGet('https://xuhaolang.cn/scripts/MainScript.lua'))("")
 end
